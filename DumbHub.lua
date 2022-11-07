64c2f9e4ef4658145b56796aec356e1358a5e6c2
WHYSTRIV3 <117491501+WHYSTRIV3@users.noreply.github.com>
2022-11-07 00:55:02 -0500

    Update DumbHub.lua

--- DumbHub.lua
+++ DumbHub.lua
@@ -1,48 +1,26 @@
 if game.PlaceId == 9522149469 then
+--Strong Clicker
+loadstring(game:HttpGet("https://raw.githubusercontent.com/WHYSTRIV3/DumbHub-LoadStrings/main/Strong%20Clickers!.Lua"))()
 
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/WHYSTRIV3/DumbHub-LoadStrings/main/Strong%20Clickers!.Lua"))()
-
-
-
-
-elseif game.PlaceId == 8750997647 then
-
+	elseif game.PlaceId == 8750997647 then
+--Tapping Simulator x
 loadstring(game:HttpGet("https://raw.githubusercontent.com/WHYSTRIV3/DumbHub-LoadStrings/main/Tapping%20Simulator%20X.lua"))()
 
-
-
-
-
-
-
-elseif game.PlaceId == 6022309314 then
-
+	elseif game.PlaceId == 6022309314 then
+--Madness Tapping
 loadstring(game:HttpGet("https://raw.githubusercontent.com/WHYSTRIV3/DumbHub-LoadStrings/main/Madness%20Tapping.lua"))()
 
-
-    
-
-
-
-
-elseif game.PlaceId == 9150032572 then
-
+	elseif game.PlaceId == 9150032572 then
+--Clicking Gods
 loadstring(game:HttpGet("https://raw.githubusercontent.com/WHYSTRIV3/DumbHub-LoadStrings/main/Clicking%20Gods.lua"))()
 
-    
-
-
-
-elseif game.PlaceId == 8884334497 then
-
+	elseif game.PlaceId == 8884334497 then
+--Mining Clicker Simulator
 loadstring(game:HttpGet("https://raw.githubusercontent.com/WHYSTRIV3/DumbHub-LoadStrings/main/Mining%20Clicker%20Simulator.lua"))()
-    
 	
-	
-elseif game.PlaceId == 9283132959 then
-
+	elseif game.PlaceId == 9283132959 then
+--Block Breaker Simulator
 loadstring(game:HttpGet("https://raw.githubusercontent.com/WHYSTRIV3/DumbHub-LoadStrings/main/Block%20Breaker%20Simulator.lua"))()	
 	
-
 
 end
