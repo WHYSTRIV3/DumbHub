94c2ef924992031d03c57b5b46dd4ef3b270daae
WHYSTRIV3 <117491501+WHYSTRIV3@users.noreply.github.com>
2022-12-10 22:28:32 -0500

    Update V2 Library.lua

--- V2 Library.lua
+++ V2 Library.lua
@@ -223,9 +223,8 @@ function library:CreateWindow(GameName)
 
 		NavigationButtonHolderUITemplatePadding.Name = "NavigationButtonHolderUITemplatePadding"
 		NavigationButtonHolderUITemplatePadding.Parent = NavigationButtonHolderTemplate
-		NavigationButtonHolderUITemplatePadding.PaddingLeft = UDim.new(0, 15)
 
-		NavigationButtonHolderUITemplatePadding.MouseButton1Click:Connect(function()
+		NavigationButtonHolderTemplate.MouseButton1Click:Connect(function()
 			for _,v in pairs(ContientContainer:GetChildren()) do
 				if v:IsA("ScrollingFrame") then
 					v.Visible = false
