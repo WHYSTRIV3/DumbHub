e8c7089c2ac8935b0e328a2e5e40e36c58765a73
WHYSTRIV3 <117491501+WHYSTRIV3@users.noreply.github.com>
2023-01-31 00:25:55 -0500

    Update UILoadStrings.lua

--- UILoadStrings.lua
+++ UILoadStrings.lua
@@ -581,6 +581,5 @@
                      elseif game.PlaceId == 6777872443 then
                 -- Pixel Piece
                 loadstring(game:HttpGet("https://raw.githubusercontent.com/WHYSTRIV3/DumbHub-LoadStrings/main/PixelPiece.lua"))()
- -----------------------------------------------------------------------------------------------------------------------------------------------------------------
     end
 
